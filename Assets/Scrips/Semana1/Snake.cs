15296dae5f47dbdc190535deb4613572bba9aaf2
ArielCarrollo <150464222+ArielCarrollo@users.noreply.github.com>
2025-03-20 16:28:13 -0500

    Snake update
    Movimiento y crecimiento de la serpiente

--- Assets/Scrips/Semana1/Snake.cs
+++ Assets/Scrips/Semana1/Snake.cs
@@ -6,6 +6,7 @@ namespace Assets.Scrips.Semana1
 {
     public class Snake : MonoBehaviour
     {
+
         public static Snake Instance { get; private set; }
 
         public int Length = 1;
@@ -14,7 +15,10 @@ namespace Assets.Scrips.Semana1
         private List<Transform> snakeSegments = new List<Transform>();
         public Transform segmentPrefab;
 
-        private Vector2 direction = Vector2.right;
+        private Vector3 direction = Vector3.right; // Cambiado a Vector3
+        private float moveDelay = 0.2f;
+        private float moveTimer = 0f;
+        private float fixedY; // Para mantener la serpiente en el plano 3D
 
         private void Awake()
         {
@@ -31,26 +35,53 @@ namespace Assets.Scrips.Semana1
         private void Start()
         {
             snakeSegments.Add(transform);
+            fixedY = transform.position.y; // Guardar la altura inicial de la serpiente
         }
 
         private void Update()
         {
-            ValidateMovement();
+            HandleInput();
+            moveTimer += Time.deltaTime;
+            if (moveTimer >= moveDelay)
+            {
+                moveTimer = 0f;
+                ValidateMovement();
+            }
         }
 
-        public void MoveSnake(Vector2 dir)
+        private void HandleInput()
         {
-            direction = dir;
+            if (Input.GetKeyDown(KeyCode.W) && direction != Vector3.back)
+                direction = Vector3.forward;
+            else if (Input.GetKeyDown(KeyCode.S) && direction != Vector3.forward)
+                direction = Vector3.back;
+            else if (Input.GetKeyDown(KeyCode.A) && direction != Vector3.right)
+                direction = Vector3.left;
+            else if (Input.GetKeyDown(KeyCode.D) && direction != Vector3.left)
+                direction = Vector3.right;
+
+            if (Input.GetKeyDown(KeyCode.UpArrow) && direction != Vector3.back)
+                direction = Vector3.forward;
+            else if (Input.GetKeyDown(KeyCode.DownArrow) && direction != Vector3.forward)
+                direction = Vector3.back;
+            else if (Input.GetKeyDown(KeyCode.LeftArrow) && direction != Vector3.right)
+                direction = Vector3.left;
+            else if (Input.GetKeyDown(KeyCode.RightArrow) && direction != Vector3.left)
+                direction = Vector3.right;
         }
 
         private void ValidateMovement()
         {
-            Vector3 newPosition = transform.position;
-            newPosition.x += direction.x;
-            newPosition.y += direction.y;
+            Vector3 newPosition = transform.position + direction;
+            newPosition.y = fixedY; // Mantiene la serpiente en el plano
             transform.position = newPosition;
 
             CheckMovement();
+
+            for (int i = snakeSegments.Count - 1; i > 0; i--)
+            {
+                snakeSegments[i].position = snakeSegments[i - 1].position;
+            }
         }
 
         private void CheckMovement()
@@ -61,6 +92,17 @@ namespace Assets.Scrips.Semana1
                 {
                     GameManager.Instance.GameOver();
                 }
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Apple"))
+            {
+                Destroy(other.gameObject);
+                UpdateLength();
+                scoreManager.UpdateScore(10);
+                GameManager.Instance.SpawnApple();
             }
         }
 
